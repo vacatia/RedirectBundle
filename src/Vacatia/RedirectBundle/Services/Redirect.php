ae197fa3d56f272b81acedef0cf474a099bc7cc5
Daniel Polito <contato@dbpolito.net>
2013-08-16 14:42:30 -0300

    Creating Service + Adding dependency

---
+++ src/Vacatia/RedirectBundle/Services/Redirect.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Vacatia\RedirectBundle\Services;
+
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Vacatia\RedirectBundle\Entity\Redirect as Entity;
+
+class Redirect
+{
+    private $em;
+    private $container;
+
+    public function __construct(EntityManager $em, ContainerInterface $container)
+    {
+        $this->em = $em;
+        $this->container = $container;
+    }
+
+    public function create($oldUrl, $newUrl, $flush = true)
+    {
+        if (empty($oldUrl) || empty($newUrl) || $oldUrl == $newUrl) {
+            return;
+        }
+
+        // Remove schemes of urls
+        $oldUrl = $this->removeScheme($oldUrl);
+        $newUrl = $this->removeScheme($newUrl);
+
+        $redirect = $this->em->getRepository('VacatiaRedirectBundle:Redirect')->findOneByOldUrl($newUrl);
+
+        if (!$redirect) {
+            $redirect = new Entity;
+            $redirect->setOldUrl($oldUrl);
+            $redirect->setNewUrl($newUrl);
+
+            $this->em->persist($redirect);
+        } else {
+            $redirect->setNewUrl($newUrl);
+        }
+
+        if ($flush === true) {
+            $this->em->flush();
+        }
+
+        return $redirect;
+    }
+
+    protected function removeScheme($url)
+    {
+        return str_replace(array('https://', 'http://'), '', $url);
+    }
+
+}
